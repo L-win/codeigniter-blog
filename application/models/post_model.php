c18bc6c84192cc1124d7feb4306463cc5ba02b88
Elvin <l-win@inbox.ru>
2018-02-07 18:45:34 +0300

    init2

--- application/models/post_model.php
+++ application/models/post_model.php
@@ -1,56 +1,58 @@
 <?php
 
 
-    class Post_model extends CI_Model {
-        
-        public function __construct(){ 
-        
-            $this->load->database();
-            
-        }
-        
-        public function get_posts( $slug = FALSE ){
-            
-            if ( $slug === FALSE ){
-                $this->db->order_by( 'id', 'DESC' );
-                $query = $this->db->get( 'posts' );
-                return $query->result_array( );
-            }
-            
-            $query = $this->db->get_where( 'posts', array( 'slug' => $slug ) );
-            return $query->row_array( );
-	    
-        }
-        
-        public function create_post(){
-            
-            $slug = url_title($this->input->post('title'));
-            
-            $data = array(
-                'title' => $this->input->post('title'),
-                'body' => $this->input->post('body'),
-                'slug' => $slug
-            );
-                    
-              return $this->db->insert( 'posts', $data );      
-        }
-        
-        public function delete_post( $id ){
-            
-            return $this->db->delete('posts', array('id'=>$id));
-            
-        }
-        
-        public function update_post( ){
-            
-            $data = array(
-                'title' => $this->input->post( 'title' ),
-                'body' => $this->input->post( 'body' )
-                    );
-            
-            $this->db->where( 'id', $this->input->post( 'id' ) );
-            return $this->db->update( 'posts', $data );
-            
-        }
-        
-    }
+	class Post_model extends CI_Model {
+	
+		public function __construct(){ 
+		
+			$this->load->database();
+			
+		}
+		
+		public function get_posts( $slug = FALSE ){
+			
+			if ( $slug === FALSE ){
+				$this->db->order_by( 'id', 'DESC' );
+				$query = $this->db->get( 'posts' );
+				return $query->result_array( );
+			}
+			
+			$query = $this->db->get_where( 'posts', array( 'slug' => $slug ) );
+			
+			return $query->row_array( );
+			
+		}
+		
+		public function create_post(){
+			
+			$slug = url_title($this->input->post('title'));
+			
+			$data = array(
+				'title' => $this->input->post('title'),
+				'body' => $this->input->post('body'),
+				'slug' => $slug
+				);
+				
+			  return $this->db->insert( 'posts', $data );	  
+		}
+		
+		public function delete_post( $id ){
+			
+			return $this->db->delete('posts', array('id'=>$id));
+			
+		}
+		
+		public function update_post( ){
+			
+			$data = array(
+				'title' => $this->input->post( 'title' ),
+				'body' => $this->input->post( 'body' )
+				);
+			
+			$this->db->where( 'id', $this->input->post( 'id' ) );
+			
+			return $this->db->update( 'posts', $data );
+			
+		}
+	
+	}
